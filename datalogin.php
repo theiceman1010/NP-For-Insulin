b1ffabf9672495b3d8da9a911066e41fbc801578
theiceman1010 <33201969+theiceman1010@users.noreply.github.com>
2019-02-26 22:44:08 -0500

    Create datalogin.php

---
+++ datalogin.php
@@ -0,0 +1,5 @@
+<?php 
+// Connects to my Database 
+mysql_connect("db4free.net", "theiceman1010@localhost", "motorola#101") or die(mysql_error()); 
+mysql_select_db("icemantestingdb") or die(mysql_error()); 
+?> 
